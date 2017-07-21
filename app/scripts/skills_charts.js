96d80aa55067d5b5ada3c99044b521e58e8dcc72
twistezo <luk89@outlook.com>
2017-07-21 12:36:37 +0200

    add skill charts from web

---
+++ app/scripts/skills_charts.js
@@ -0,0 +1,57 @@
+    $( document ).ready(function() {
+        $("#circle-java").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#2aabd2',
+            textBelow: true,
+            percent: 85,
+            text: 'Technologie Java',
+        });
+            $("#circle-rust").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#2aabd2',
+            textBelow: true,
+            percent: 60,
+            text: 'Technologie Rust',
+        });
+            $("#circle-web").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#2aabd2',
+            textBelow: true,
+            percent: 80,
+            text: 'Web',
+        });
+           $("#circle-graphics").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#2aabd2',
+            textBelow: true,
+            percent: 95,
+            text: 'Grafika',
+        });
+            $("#circle-mobile").circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#2aabd2',
+            textBelow: true,
+            percent: 50,
+            text: 'Android',
+        });
+});
