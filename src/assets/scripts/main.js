7095e8c234946ed3daf8c04ae6bb56f33fd051a6
twistezo <twistezo7@gmail.com>
2017-09-11 19:54:37 +0200

    delete vibrations :)

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -26,11 +26,5 @@
     $('img, .circles').addClass('bounce-top');
     $('h3').addClass('bounce-left');
 
-    $('img').hover(function(){
-      $(this).addClass('vibrate-1');
-    }, function(){
-      $(this).removeClass('vibrate-1 bounce-top');
-    });
-    
   });
 })();
