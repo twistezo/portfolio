4149cb50f4c2a3007a84a1fc96816df7d8378e13
twistezo <twistezo7@gmail.com>
2017-09-12 20:15:46 +0200

    deploy on github page

---
+++ docs/assets/scripts/skills_charts.js
@@ -0,0 +1,75 @@
+(function () {
+    $( document ).ready(function() {
+        var textYPos = 195;
+        $('#circle-java').circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#0f0f0f',
+            textBelow: true,
+            percent: 75,
+            text: 'Technologie Java',
+            textColor: '#0f0f0f',
+            percentageY: 105,
+            textY: textYPos,
+        });
+            $('#circle-rust').circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#0f0f0f',
+            textBelow: true,
+            percent: 84,
+            text: 'Technologie Rust',
+            textColor: '#0f0f0f',
+            percentageY: 105,
+            textY: textYPos,
+        });
+            $('#circle-web').circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#0f0f0f',
+            textBelow: true,
+            percent: 84,
+            text: 'Web',
+            textColor: '#0f0f0f',
+            percentageY: 105,
+            textY: textYPos,
+        });
+            $('#circle-graphics').circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#0f0f0f',
+            textBelow: true,
+            percent: 90,
+            text: 'Grafika',
+            textColor: '#0f0f0f',
+            percentageY: 105,
+            textY: textYPos,
+        });
+            $('#circle-mobile').circliful({
+            animation: 1,
+            animationStep: 5,
+            foregroundBorderWidth: 10,
+            backgroundBorderWidth: 5,
+            foregroundColor: '#2aabd2',
+            fontColor: '#0f0f0f',
+            textBelow: true,
+            percent: 59,
+            text: 'Mobile',
+            textColor: '#0f0f0f',
+            percentageY: 105,
+            textY: textYPos,
+        });
+    });
+})();
