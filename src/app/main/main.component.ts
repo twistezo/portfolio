de809c55118d24c4da1989496cb5f28a7f9fbb80
twistezo <twistezo7@gmail.com>
2017-09-17 17:50:43 +0200

    add login button add info if logged

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../firebase-auth/auth.service';
 
 @Component({
     selector: 'app-main',
     templateUrl: '../main/main.component.html',
- })
+})
 
- export class MainComponent { }
+export class MainComponent {
+    constructor(public authService: AuthService) { }
+}
--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../firebase-auth/auth.service';
 
 @Component({
     selector: 'app-template',
     templateUrl: '../template/template.component.html'
- })
+})
 
- export class TemplateComponent { }
+export class TemplateComponent {
+    constructor(public authService: AuthService) { }
+}
