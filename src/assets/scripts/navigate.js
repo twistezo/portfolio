b08c0875a6bfc7636f5ae754af539f903f3b9cd1
twistezo <twistezo7@gmail.com>
2017-09-17 13:25:44 +0200

    add parallax

--- src/assets/scripts/navigate.js
+++ src/assets/scripts/navigate.js
@@ -2,13 +2,13 @@
   $(document).ready(function () {
 
     // bs4 scrollspy smooth
-    $(".nav-item a").on('click', function(event) {
+    $(".nav-item a").on('click', function (event) {
       if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
         $('html, body').animate({
           scrollTop: $(hash).offset().top
-        }, 400, function() {
+        }, 400, function () {
           window.location.hash = hash;
         });
       }
@@ -16,11 +16,11 @@
 
     // back to top button
     $('#back-to-top').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
+      $('body,html').animate({
+        scrollTop: 0
+      }, 400);
+      return false;
     });
-  
+
   });
 })();
