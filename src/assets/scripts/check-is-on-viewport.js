b6e14895a352a0c805e0d15133295f8a9d78aa74
twistezo <twistezo7@gmail.com>
2017-09-20 09:53:47 +0200

    add animation depends on viewport

--- src/assets/scripts/check-is-on-viewport.js
+++ src/assets/scripts/check-is-on-viewport.js
@@ -15,25 +15,51 @@
         // class="test" -> '.test'
         // <h3> -> 'h3'
         $(window).on('resize scroll', function () {
-            if (!isPlayed) {
-                hover_circles();
+            if (!isAnimCirclesPlayed) {
+                animCircles();
             }
-
+            if (!isAnimCardsPlayed) {
+                animCards();
+            }
+            if (!isContactButtonPlayed) {
+                animContactButton();
+            }
         });
 
-        function hover_circles() {
+        var isAnimCirclesPlayed = false;
+        function animCircles() {
             if ($('.c100').isInViewport()) {
-                $('.c100').addClass('hover');
-                console.log('jest');
-                window.setTimeout(remove, 750);
-                isPlayed = true;
-
+                $('.c100').addClass('rotate-in-center');
+                window.setTimeout(removeAnimCircles, 1000);
+                isAnimCirclesPlayed = true;
             }
         }
+        function removeAnimCircles() {
+            $('.c100').removeClass('rotate-in-center');
+        };
 
-        var isPlayed = false;
-        function remove() {
-            $('.c100').removeClass('hover');
+        var isAnimCardsPlayed = false;
+        function animCards() {
+            if ($('.card').isInViewport()) {
+                $('.card').addClass('scale-in-ver-center');
+                window.setTimeout(remove_anim_circles, 1000);
+                isAnimCardsPlayed = true;
+            }
+        }
+        function removeAnimCards() {
+            $('.card').removeClass('scale-in-ver-center');
+        };
+
+        var isContactButtonPlayed = false;
+        function animContactButton() {
+            if ($('#section5').isInViewport()) {
+                $('.btn').addClass('heartbeat');
+                window.setTimeout(removeAnimContactButton, 5000);
+                isContactButtonPlayed = true;
+            }
+        }
+        function removeAnimContactButton() {
+            $('.btn').removeClass('heartbeat');
         };
 
     });
