17feaef42c0c05cdd575cf2386b939d7e485903b
twistezo <twistezo7@gmail.com>
2017-10-01 10:47:28 +0200

    change firebase project name change firebase host url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCH00lpemCZKMQ56Cjm4VAG3teNVTe7ReI',
-    authDomain: 'web-portfolio-f37c9.firebaseapp.com',
-    databaseURL: 'https://web-portfolio-f37c9.firebaseio.com',
-    projectId: 'web-portfolio-f37c9',
-    storageBucket: 'web-portfolio-f37c9.appspot.com',
-    messagingSenderId: '791290022700'
+    apiKey: 'AIzaSyAtZN0af5Xuf3vDLvav7cyjSrH0yuCyLzY',
+    authDomain: 'twistezo-portfolio.firebaseapp.com',
+    databaseURL: 'https://twistezo-portfolio.firebaseio.com',
+    projectId: 'twistezo-portfolio',
+    storageBucket: 'twistezo-portfolio.appspot.com',
+    messagingSenderId: '905549320087'
   }
 };
