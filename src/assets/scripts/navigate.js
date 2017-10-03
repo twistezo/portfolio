56a33e7de7232202e38ab3f2aa02111c09aab2e7
twistezo <twistezo7@gmail.com>
2017-10-03 21:25:46 +0200

    add cookie warning

--- src/assets/scripts/navigate.js
+++ src/assets/scripts/navigate.js
@@ -1,7 +1,7 @@
 (function () {
   $(document).ready(function () {
 
-    // bs4 scrollspy smooth
+    // bootstrap 4 smooth scrollspy 
     $(".nav-item a").on('click', function (event) {
       if (this.hash !== "") {
         event.preventDefault();
@@ -26,6 +26,51 @@
     $('.navbar-nav>li>.hide-on-click').on('click', function () {
       $('.navbar-collapse').collapse('hide');
     });
+
+    // cookie warning behaviour
+    var cookieWarningName = `cookie-warning`;
+
+    if (readCookie(cookieWarningName) != 1) {
+      showCookieWarning();
+      hideOnClickCookieWarning();
+      createCookie(cookieWarningName, 1, 30);
+    }
+
+    function hideOnClickCookieWarning() {
+      $('.btn-cookie-warning').on('click', function () {
+        $('.cookie-warning').addClass('hidden');
+      });
+    }
+
+    function showCookieWarning() {
+      $('.cookie-warning').removeClass('hidden');
+    }
+
+    // cookies
+    function createCookie(name, value, days) {
+      var expires = "";
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+
+    function eraseCookie(name) {
+      createCookie(name, "", -1);
+    }
 
   });
 })();
