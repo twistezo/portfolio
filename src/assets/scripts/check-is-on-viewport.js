6ee35d84738f577f021bd70b02e07da075dc7b38
twistezo <twistezo7@gmail.com>
2017-10-22 19:05:23 +0200

    -remove circle-progress -add svg skill icons

--- src/assets/scripts/check-is-on-viewport.js
+++ src/assets/scripts/check-is-on-viewport.js
@@ -14,8 +14,8 @@
         // class="test" -> '.test'
         // <h3> -> 'h3'
         $(window).on('resize scroll', function () {
-            if (!isAnimCirclesPlayed) {
-                animCircles();
+            if (!isSkillsPlayed) {
+                animSkills();
             }
             if (!isAnimCardsPlayed) {
                 animCards();
@@ -25,43 +25,28 @@
             }
         });
 
-        var isAnimCirclesPlayed = false;
-        function animCircles() {
-            if ($('.skill-circle').isInViewport()) {
-                createCircle('.java-circle', 60);
-                createCircle('.rust-circle', 80);
-                createCircle('.web-circle', 70);
-                createCircle('.mobile-circle', 50);
-                createCircle('.graphics-circle', 90);
-
-                function createCircle(divId, percent) {
-                    var circleSize = 80;
-                    $(divId).circleProgress({
-                        value: percent * 0.01,
-                        fill: '#2aabd2',
-                        size: circleSize,
-                        emptyFill: 'rgba(119, 119, 119, 1)',    // #777
-                        startAngle: -Math.PI / 2,
-                        animation: { duration: 1600, easing: "circleProgressEasing" },
-                        insertMode: 'append',
-                    }).on('circle-animation-progress', function (event, progress, stepValue) {
-                        $(this).find('strong').html(Math.round(100 * stepValue) + '<span>%</span>');
-                    });
-                }
-                isAnimCirclesPlayed = true;
+        var isSkillsPlayed = false;
+        function animSkills() {
+            if ($('.skill-logo').isInViewport()) {
+                $('.skill-logo').addClass('scale-in-center');
+                window.setTimeout(removeAnimSkills, 1000);
+                isSkillsPlayed = true;
             }
         }
+        function removeAnimSkills() {
+            $('.skill-logo').removeClass('scale-in-center');
+        };
 
         var isAnimCardsPlayed = false;
         function animCards() {
             if ($('.card').isInViewport()) {
-                $('.card').addClass('scale-in-ver-center');
+                $('.card').addClass('scale-in-center');
                 window.setTimeout(removeAnimCards, 1000);
                 isAnimCardsPlayed = true;
             }
         }
         function removeAnimCards() {
-            $('.card').removeClass('scale-in-ver-center');
+            $('.card').removeClass('scale-in-center');
         };
 
         var isContactButtonPlayed = false;
