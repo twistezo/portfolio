2d52eae4babf31f51c5a2b2effa1c360bf3eddfd
twistezo <twistezo7@gmail.com>
2018-03-25 23:29:36 +0200

    create js classes instead of render methods

--- src/app/components/layout.js
+++ src/app/components/layout.js
@@ -1,5 +1,4 @@
 import { html, render } from 'lit-html';
-import $ from 'jquery';
 import AOS from 'aos';
 import 'bootstrap/dist/js/bootstrap.js';
 
@@ -17,40 +16,31 @@ import '../styles/layout.css';
 export default class App {
     constructor() {
         AOS.init();
-        enableHashSmoothScrolling();
+        this.navbar = new Navbar().NavbarComponent();
         const el = document.querySelector('#app');
-        render(Layout(''), el);
+        render(this.Layout(''), el);
     }
-}
 
-function enableHashSmoothScrolling() {
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 500);
-    });
+    Layout = () => {
+        return html`
+        <div>
+            <div id="about-me" class="sectionPaddingTop fullHeight aosScrollbarFix">
+                ${AboutMe}
+            </div>
+            ${this.navbar}
+            <div id="skills" class="sectionPaddingTop aosScrollbarFix">
+                ${Skills}
+            </div>
+            <div id="experience" class="sectionPaddingTop aosScrollbarFix">
+                ${Experience}
+            </div>
+            <div id="projects" class="sectionPaddingTop aosScrollbarFix">
+                ${Projects}
+            </div>
+            <div id="contact" class="sectionPaddingTop aosScrollbarFix">
+                ${Contact}
+            </div>
+        </div>
+      `;
+    };
 }
-
-const Layout = () => {
-    return html`
-    <div>
-        <div id="about-me" class="sectionPaddingTop fullHeight aosScrollbarFix">
-            ${AboutMe}
-        </div>
-        ${Navbar}
-        <div id="skills" class="sectionPaddingTop aosScrollbarFix">
-            ${Skills}
-        </div>
-        <div id="experience" class="sectionPaddingTop aosScrollbarFix">
-            ${Experience}
-        </div>
-        <div id="projects" class="sectionPaddingTop aosScrollbarFix">
-            ${Projects}
-        </div>
-        <div id="contact" class="sectionPaddingTop aosScrollbarFix">
-            ${Contact}
-        </div>
-    </div>
-  `;
-};
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -2,29 +2,48 @@ import { html } from 'lit-html';
 
 import '../styles/navbar.css';
 
-const Navbar =
-    html`
-    <div class="navbar stickyNavbar navbar-expand-lg">
-        <div>
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#about-me">About-me</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#skills">Skills</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#experience">Experience</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#projects">Projects</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-`;
+export default class Navbar {
+    constructor() {
+        this.smoothScrolling();
+    }
 
-export default Navbar;
+    smoothScrolling() {
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 500);
+        });
+    }
+
+    NavbarComponent = () => {
+        return html`
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark stickyNavbar">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about-me">About-me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#skills">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#experience">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    `;
+    }
+}
